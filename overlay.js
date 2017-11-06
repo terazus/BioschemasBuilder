80fa28de50b48804e804623a1a1307240764b6e1
Terazus <batistadominique@hotmail.com>
2017-11-06 10:17:48 +0100

    Revomed useless folder

--- overlay.js
+++
@@ -1,7 +0,0 @@
-function openNav(){
-	document.getElementById("mainContainer").style.left = "-3500px";
-	document.getElementById("mainContainer").style.opacity = "0";
-	document.getElementById("editRequired").style.left = "0";
-	document.getElementById("editRequired").style.right = "0";
-	document.getElementById("editRequired").style.opacity = "1";
-}
